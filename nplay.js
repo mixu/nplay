d551fc07f00d37a35e6114e46ea1abb3fe12e292
Mikito Takada <mikito.takada@gmail.com>
2014-03-12 19:02:22 -0700

    Add list

--- nplay.js
+++ nplay.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
 var fs = require('fs'),
     path = require('path'),
+    List = require('minitask').list,
+    Meta = require('./lib/metadata.js'),
     player = require('./lib/player.js');
 
 var existsSync = (fs.existsSync ? fs.existsSync : path.existsSync),
     homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
 
+var list = new List();
+
+var isList = process.argv.indexOf('--ls') > -1;
+
 if(process.argv.length > 2) {
   var args = process.argv.slice(0);
   // shift off node and script name
   args.shift(); args.shift();
-  console.log('Adding paths: ', args);
-  args.forEach(function(dir) { player.scan(dir); });
+  args.forEach(function(dir) {
+    var fulldir = path.resolve(homePath, dir);
+    if(fs.existsSync(fulldir)) {
+      if(!isList) {
+        console.log('Adding path: ', fulldir);
+      }
+      list.add(fulldir);
+    }
+  });
 } else {
   if(!existsSync(homePath+'/.nplay.json')) {
     console.log('~/.nplay.json does not exist.');
@@ -21,9 +34,24 @@ if(process.argv.length > 2) {
     process.exit();
   } else {
     require(homePath+'/.nplay.json').directories.forEach(function(dir) {
-      player.scan(dir);
+      list.add(path.resolve(homePath, dir));
     });
   }
 }
 
-player.run();
+list.exec(function(err, files) {
+  if (isList) {
+    Meta.read(homePath+'/.nplay.db.json');
+    files.map(function(file) {
+      var basename = path.basename(file.name, '.mp3'),
+          meta = Meta.get(basename),
+          rating = (meta && meta.rating ? meta.rating : 0);
+
+      if(file.stat.isFile() && rating > 3) {
+        console.log(file.name);
+      }
+    })
+    return;
+  }
+  player.run(files);
+});
