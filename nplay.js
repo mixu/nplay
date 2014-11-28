81b9f56cacab367631816c3db6df148779df9756
Mikito Takada <mikito.takada@gmail.com>
2014-11-27 17:49:16 -0800

    Refactor and modernize code

--- nplay.js
+++ nplay.js
@@ -1,57 +1,59 @@
 #!/usr/bin/env node
 var fs = require('fs'),
     path = require('path'),
-    List = require('minitask').list,
-    Meta = require('./lib/metadata.js'),
-    player = require('./lib/player.js');
+    Meta = require('./lib/metadata.js');
 
 var existsSync = (fs.existsSync ? fs.existsSync : path.existsSync),
-    homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+    isList = process.argv.indexOf('--ls') > -1,
+    homePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'],
+    shouldReadConfig = (process.argv.length <= 2 || isList);
 
-var list = new List();
-
-var isList = process.argv.indexOf('--ls') > -1;
-
-if(process.argv.length > 2 && !isList) {
-  var args = process.argv.slice(0);
-  // shift off node and script name
-  args.shift(); args.shift();
-  args.forEach(function(dir) {
-    var fulldir = path.resolve(homePath, dir);
-    if(fs.existsSync(fulldir)) {
-      if(!isList) {
-        console.log('Adding path: ', fulldir);
-      }
-      list.add(fulldir);
-    }
-  });
-} else {
-  if(!existsSync(homePath+'/.nplay.json')) {
-    console.log('~/.nplay.json does not exist.');
-    console.log('Please create "'+homePath+'/.nplay.json" with paths to your music directories, like this:');
-    console.log('\techo \'{ "directories": [ "/home/m/mp3" ] }\' >> ~/.nplay.json');
-    console.log('Alternatively, specify paths as arguments: nplay /home/m/mp3');
-    process.exit();
-  } else {
-    require(homePath+'/.nplay.json').directories.forEach(function(dir) {
-      list.add(path.resolve(homePath, dir));
-    });
-  }
+if (shouldReadConfig && !existsSync(homePath+'/.nplay.json')) {
+  console.log('~/.nplay.json does not exist.');
+  console.log('Please create "'+homePath+'/.nplay.json" with paths to your music directories, like this:');
+  console.log('\techo \'{ "directories": [ "/home/m/mp3" ] }\' >> ~/.nplay.json');
+  console.log('Alternatively, specify paths as arguments: nplay /home/m/mp3');
+  process.exit();
 }
 
-list.exec(function(err, files) {
-  if (isList) {
-    Meta.read(homePath+'/.nplay.db.json');
-    files.map(function(file) {
-      var basename = path.basename(file.name, '.mp3'),
-          meta = Meta.get(basename),
-          rating = (meta && meta.rating ? meta.rating : 0);
+var pi = require('pipe-iterators'),
+    dirStream = require('./lib/dir-stream'),
+    validExts = { '.mp3': true, '.wav': true, '.m4a': true },
+    dirs = (shouldReadConfig ? require(homePath+'/.nplay.json').directories : process.argv.slice(2).filter(function(str) { return str != '--ls'; }));
 
-      if(file.stat.isFile()) {
-        console.log(rating + ',' + file.name);
-      }
-    })
-    return;
-  }
-  player.run(files);
-});
+
+var listen = require('./lib/listen.js'),
+    commandMode = require('./lib/modes/command.js'),
+    Playlist = require('./lib/playlist.js');
+
+Meta.read();
+
+pi.fromArray(dirs)
+  .pipe(pi.map(function(dir) {
+    return path.resolve(homePath, dir);
+  }))
+  .pipe(dirStream()) // resolve to files { path: .., stat: .. }
+  .pipe(pi.filter(function(file) { return validExts[path.extname(file.path)]; }))
+  .pipe(pi.map(function(file) {
+    // add rating property from metadata
+    var name = path.basename(file.path, '.mp3'),
+        meta = Meta.get(name);
+    file.rating = (meta && meta.rating ? meta.rating : 0);
+    return file;
+  }))
+  .pipe(pi.toArray(function(files) {
+    if (isList) {
+      files.forEach(function(file) {
+        console.log(file.rating + ',' + file.path);
+      });
+      return;
+    }
+
+    var playlist = new Playlist(files);
+
+
+    listen(
+      commandMode({ playlist: playlist })
+    );
+
+  }));
